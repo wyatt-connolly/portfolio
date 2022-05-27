997ade96b21349465d28f7971ca64db002afb9b1
MassyWeeWee <wyattconnollyfitness@gmail.com>
2022-05-26 18:51:44 -0700

    hero section

---
+++ components/About.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container mx-auto px-6 w-full border-solid border-2 border-indigo-600 h-screen ">
+      <h1 className="text-5xl underline text-center">About Me</h1>
+      <p className="text-lg">
+        Welcome! My name is Wyatt Connolly, I am a self-taught developer and web
+        designer <br /> Starting from no-code in January of 2020 I became
+        obsessed with the world of programming. Poring through resources such as
+        The Odin Project, LeetCode, and Codecademy I have been developing the
+        necessary skills for becoming a full stack developer.
+        <br /> &gt;Let&apos;s work together!
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ components/Hero.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactTypingEffect from "react-typing-effect";
+import { BriefcaseIcon, CodeIcon } from "@heroicons/react/solid";
+
+function Hero() {
+  return (
+    <div className="container mx-auto max-w-2xl px-4 md:px-8 py-40 md:py-48">
+      <div className="flex items-center space-x-3">
+        <img
+          src="profile.jpg"
+          alt="profile-img"
+          className="h-16 w-16 rounded-full ring-4 ring-[#3E3E69]"
+        />
+        <div className="pl-3 space-y-2">
+          <h1 className="tracking-widest text-3xl font-bold text-[#A1A1B5]">
+            WYATT
+          </h1>
+          <h2 className="text-xl text-[#A1A1B5]">
+            <ReactTypingEffect
+              text={["Developer", "Designer"]}
+              speed={100}
+              typingDelay={1000}
+            />
+          </h2>
+        </div>
+      </div>
+      <p className="text-[#A1A1B5] text-xl py-6">
+        Welcome to my digital garden where I share what I'm learning about
+        shipping great products, becoming a better developer and growing a
+        career in tech.
+      </p>
+      <div className="flex px-1 mt-4 md:mt-12 gap-12">
+        <div className="flex flex-col items-center space-y-1 cursor-pointer transition ease-in-out delay-150 hover:scale-125 hover:duration-300 ">
+          <div className="flex items-center justify-center rounded h-8 w-8 bg-gradient-to-r from-[#3E3E69] to-[#A1A1B5] ">
+            <BriefcaseIcon className="rounded h-6 w-6 fill-white " />
+          </div>
+          <p className="text-[#A1A1B5] text-md">LinkedIn</p>
+        </div>
+        <div className="flex flex-col items-center space-y-1 cursor-pointer transition ease-in-out delay-150 hover:scale-125 hover:duration-300 ">
+          <div className="flex items-center justify-center rounded h-8 w-8 bg-gradient-to-r from-[#3E3E69] to-[#A1A1B5] ">
+            <CodeIcon className="rounded h-6 w-6 fill-white " />
+          </div>
+          <p className="text-[#A1A1B5] text-md">Github</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
